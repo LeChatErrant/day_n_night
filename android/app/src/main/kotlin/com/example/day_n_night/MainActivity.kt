dc2ffe46abed4cfd1f00db7eceee69d39ea96714
LeChatErrant <guillaume.hector@epitech.eu>
2021-10-01 18:28:42 +0200

    feat: basic flutter app

---
+++ android/app/src/main/kotlin/com/example/day_n_night/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.day_n_night
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Day\'n\'night',
+      theme: ThemeData(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
